95603c47ef107b6d98ef5ebdb97f96320dddd92b
zoujunlin <1927007992@qq.com>
2018-12-27 15:48:04 +0800

    config文件以及文件接口

---
+++ CuteWeb/CuteWeb/InfoApi.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/27 15:35
+# @Author  : ZouJunLin
+"""
+连接配置文件的数据库
+"""
+import ConfigParser
+
+
+class InfoAPI:
+    def __init__(self):
+        self.cf=ConfigParser.ConfigParser()
+        self.cf.read("./config.ini")
+
+    def GetStockDatabase(self):
+        """分别返回server,user,password,dbname"""
+        temp=self.cf.get("db","stock").split(",")
+        return temp[0],temp[1],temp[2],temp[3]
+
+
+
+# if __name__=='__main__':
+#     print InfoAPI().GetStockDatabase()
